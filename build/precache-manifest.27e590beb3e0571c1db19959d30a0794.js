604083f10084d84655f86f5dd8a691375fead083
LiamJack1995 <liam2dan@hotmail.co.uk>
2020-06-07 15:57:10 +0100

    Added build

---
+++ build/precache-manifest.27e590beb3e0571c1db19959d30a0794.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cda9eb7650550eba368e65403ee04d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e8107cab10a439ceaee",
+    "url": "/static/css/main.80cce685.chunk.css"
+  },
+  {
+    "revision": "55fbedc665ae864edb6b",
+    "url": "/static/js/2.3676b2fa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3676b2fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e8107cab10a439ceaee",
+    "url": "/static/js/main.5685ce60.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "e63d12ac21ae2e235e9fb40765214b8e",
+    "url": "/static/media/Liam.e63d12ac.JPG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
